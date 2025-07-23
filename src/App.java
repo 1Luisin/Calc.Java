64e85e1aa5a81e7e38389f4cd42f405de92980ee
Luis Guilherme <guilherme32698@gmail.com>
2025-07-23 16:17:16 -0300

    montando a aplicação com variaveis e entrada de dados

--- src/App.java
+++ src/App.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
+        Scanner leitura = new Scanner(System.in); // instanciando o objeto Scanner usando um dispositivo de entrada (System.in)
+        
+        Double soma1; // Váriavel
+        Double soma2; // Váriavel 2
         System.out.println("bem vindo a calculadora!");
+        System.out.println("Informe um número para adicionar a outro");
+
+        soma1 = leitura.nextDouble();
+
+        System.out.println("Informe o segundo número.");
+
+        soma2 = leitura.nextDouble();
+
+        Double ResultadoSoma = soma1 + soma2;
+    
     }
 }
